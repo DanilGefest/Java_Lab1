81a1eed0ea274c6d994cf2d38d2fb68c1fb979c9
Danil <dzemcev@bk.ru>
2024-09-18 22:19:16 +0700

    Ni4ego ne rabotaet

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,41 @@
 package org.example;
+import java.util.ArrayList;
+import java.util.List;
 
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
         System.out.println(inc(number));
+        FizzBuzzMethod fb = new FizzBuzzMethod();
+        List<String> list = fb.generateFizzBuzzList();
+        list.forEach(System.out::println);
     }
 
+
+    public class FizzBuzzMethod {
+        public List<String> generateFizzBuzzList() {
+            List<String> result = new ArrayList<>();
+            for (int i = 1; i <= 500; i++) {
+                if (i % 5 == 0 && i % 7 == 0) {
+                    result.add("FizzBuzz");
+                } else if (i % 5 == 0) {
+                    result.add("Fizz");
+                } else if (i % 7 == 0) {
+                    result.add("Buzz");
+                } else {
+                    result.add(Integer.toString(i));
+                }
+            }
+            return result;
+        }
+
+    }
     public static int inc(int number){
         return number + 1;
     }
 }
+
+
